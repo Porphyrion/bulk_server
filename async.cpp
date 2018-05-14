bf75c5aeff6689f1a116e7513834a3f86c59a9f1
Ivan <ivanbasurman88@yandex.ru>
2018-05-14 22:03:46 +0300

    omt

--- async.cpp
+++ async.cpp
@@ -7,31 +7,31 @@ handle_t async::connect(std::size_t bulk_){
     return static_cast<handle_t>(h.get());
 };
 
-void async::receive(handle_t handle, const char *data, std::size_t size){
-    auto x = reinterpret_cast<Handle*>(handle);
+void async::receive(async::handle_t handle, const char *data, std::size_t size){
+    auto x = reinterpret_cast<async::Handle*>(handle);
     x->readString(data, size);
 };
 
-void async::disconnect(handle_t handle) {
+void async::disconnect(async::handle_t handle) {
     auto x = std::find_if(handlers.begin(), handlers.end(), [&handle](decltype(*handlers.begin()) i){return i.get() == handle;} );
     x->get()->lastBulk();
     handlers.erase(x);
 };
 
-void async::receive(handle_t handle, std::string line){
-    auto x = reinterpret_cast<Handle*>(handle);
+void async::receive(async::handle_t handle, std::string line){
+    auto x = reinterpret_cast<async::Handle*>(handle);
     x->readString(line);
 };
 
-void async::receive(handle_t handle, bulk_message bulk_){
-    auto x = reinterpret_cast<Handle*>(handle);
+void async::receive(async::handle_t handle, bulk_message bulk_){
+    auto x = reinterpret_cast<async::Handle*>(handle);
     bulk_.push_back("}");
     for(auto msg : bulk_){
         x->readString(msg);
     }
 };
 
-void async::last_session_bulk(handle_t handle){
-    auto x = reinterpret_cast<Handle*>(handle);
+void async::last_session_bulk(async::handle_t handle){
+    auto x = reinterpret_cast<async::Handle*>(handle);
     x->lastBulk();
 };
